9ba9cb74d45d08fc6d43b9f618f2f33330be00c8
sheriff10 <ibrahimsheriff999@gmail.com>
2024-07-20 20:06:32 +0100

    changed bg4

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
                 <p className='uppercase text-xl'>A solana memecoin</p>
                 <h1 className='lg:text-8xl text-4xl font-extrabold my-5 mt-3 font-jumb'>MEet $CHUCHU</h1>
                 <span className='italic text-3xl'>Achi is the dog in the $WIF meme Chuchu is cute, we believe she deserves a billion too. <br /> Evidence on @ma_babezz on instagram            </span>
-            </div>
+            </div>   
 
             <div className="btn-wrap text-center my-10 ">
                 <button className='p-4 px-7 border border-black font-jumb text-red-800'>Buy Now</button>
